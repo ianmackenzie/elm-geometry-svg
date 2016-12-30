13a20a86264ec78cf1567088bdc53001010d7c33
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-12-30 17:18:36 -0500

    Add initial module documentation

---
+++ examples/DocumentationExamples.elm
@@ -0,0 +1,336 @@
+module DocumentationExamples exposing (..)
+
+import Svg exposing (Svg)
+import Svg.Attributes as Attributes
+import OpenSolid.Svg as Svg
+import OpenSolid.Geometry.Types exposing (..)
+import OpenSolid.Point2d as Point2d
+import OpenSolid.Direction2d as Direction2d
+import OpenSolid.Frame2d as Frame2d
+import Html exposing (Html)
+
+
+pointSvg : Svg Never
+pointSvg =
+    Svg.point2d
+        [ Attributes.r "10"
+        , Attributes.fill "orange"
+        , Attributes.stroke "blue"
+        , Attributes.strokeWidth "2"
+        ]
+        (Point2d ( 150, 150 ))
+
+
+lineSegmentSvg : Svg Never
+lineSegmentSvg =
+    Svg.lineSegment2d
+        [ Attributes.stroke "blue"
+        , Attributes.strokeWidth "5"
+        ]
+        (LineSegment2d
+            ( Point2d ( 100, 100 )
+            , Point2d ( 200, 200 )
+            )
+        )
+
+
+triangleSvg : Svg Never
+triangleSvg =
+    Svg.triangle2d
+        [ Attributes.stroke "blue"
+        , Attributes.strokeWidth "10"
+        , Attributes.strokeLinejoin "round"
+        , Attributes.fill "orange"
+        ]
+        (Triangle2d
+            ( Point2d ( 100, 100 )
+            , Point2d ( 200, 100 )
+            , Point2d ( 100, 200 )
+            )
+        )
+
+
+polylineSvg : Svg Never
+polylineSvg =
+    Svg.polyline2d
+        [ Attributes.stroke "blue"
+        , Attributes.fill "none"
+        , Attributes.strokeWidth "5"
+        , Attributes.strokeLinecap "round"
+        , Attributes.strokeLinejoin "bevel"
+        ]
+        (Polyline2d
+            [ Point2d ( 100, 100 )
+            , Point2d ( 120, 200 )
+            , Point2d ( 140, 100 )
+            , Point2d ( 160, 200 )
+            , Point2d ( 180, 100 )
+            , Point2d ( 200, 200 )
+            ]
+        )
+
+
+polygonSvg : Svg Never
+polygonSvg =
+    Svg.polygon2d
+        [ Attributes.stroke "blue"
+        , Attributes.fill "orange"
+        , Attributes.strokeWidth "5"
+        ]
+        (Polygon2d
+            [ Point2d ( 100, 200 )
+            , Point2d ( 120, 150 )
+            , Point2d ( 180, 150 )
+            , Point2d ( 200, 200 )
+            ]
+        )
+
+
+crosshair : Point2d -> Svg Never
+crosshair point =
+    let
+        ( x, y ) =
+            Point2d.coordinates point
+
+        offset =
+            6
+
+        vertical =
+            LineSegment2d
+                ( Point2d ( x, y - offset )
+                , Point2d ( x, y + offset )
+                )
+
+        horizontal =
+            LineSegment2d
+                ( Point2d ( x - offset, y )
+                , Point2d ( x + offset, y )
+                )
+    in
+        Svg.g []
+            [ Svg.point2d [ Attributes.r "2", Attributes.fill "black" ] point
+            , Svg.g [ Attributes.strokeWidth "1", Attributes.stroke "black" ]
+                (List.map (Svg.lineSegment2d []) [ vertical, horizontal ])
+            ]
+
+
+scaledSvg : Svg Never
+scaledSvg =
+    let
+        scales =
+            [ 1.0, 1.5, 2.5 ]
+
+        referencePoint =
+            Point2d ( 100, 100 )
+
+        scaledPoint : Float -> Svg Never
+        scaledPoint scale =
+            Svg.scaleAbout referencePoint scale pointSvg
+    in
+        Svg.g []
+            (crosshair referencePoint :: List.map scaledPoint scales)
+
+
+rotatedSvg : Svg Never
+rotatedSvg =
+    let
+        angles =
+            List.map degrees [ 0, 30, 60, 90, 120, 150 ]
+
+        referencePoint =
+            Point2d ( 200, 150 )
+
+        rotatedPoint : Float -> Svg Never
+        rotatedPoint angle =
+            Svg.rotateAround referencePoint angle pointSvg
+    in
+        Svg.g []
+            (crosshair referencePoint :: List.map rotatedPoint angles)
+
+
+translatedSvg : Svg Never
+translatedSvg =
+    Svg.g []
+        [ polylineSvg
+        , Svg.translateBy (Vector2d ( 0, 40 )) polylineSvg
+        , Svg.translateBy (Vector2d ( 5, -60 )) polylineSvg
+        ]
+
+
+axis2d : Axis2d -> Svg Never
+axis2d axis =
+    Svg.lineSegment2d
+        [ Attributes.strokeWidth "0.5"
+        , Attributes.stroke "black"
+        , Attributes.strokeDasharray "3 3"
+        ]
+        (LineSegment2d
+            ( Point2d.along axis 50
+            , Point2d.along axis 250
+            )
+        )
+
+
+mirroredSvg : Svg Never
+mirroredSvg =
+    let
+        horizontalAxis =
+            Axis2d
+                { originPoint = Point2d ( 0, 220 )
+                , direction = Direction2d.x
+                }
+
+        angledAxis =
+            Axis2d
+                { originPoint = Point2d ( 0, 150 )
+                , direction = Direction2d.fromAngle (degrees -10)
+                }
+    in
+        Svg.g []
+            [ polygonSvg
+            , axis2d horizontalAxis
+            , Svg.mirrorAcross horizontalAxis polygonSvg
+            , axis2d angledAxis
+            , Svg.mirrorAcross angledAxis polygonSvg
+            ]
+
+
+direction2d : Point2d -> Direction2d -> Svg msg
+direction2d basePoint direction =
+    let
+        frame =
+            Frame2d
+                { originPoint = basePoint
+                , xDirection = direction
+                , yDirection = Direction2d.perpendicularTo direction
+                }
+
+        tipPoint =
+            Point2d ( 50, 0 )
+
+        stemPoint =
+            Point2d ( 45, 0 )
+
+        leftPoint =
+            Point2d ( 45, 3 )
+
+        rightPoint =
+            Point2d ( 45, -3 )
+
+        stem =
+            LineSegment2d ( Point2d.origin, stemPoint )
+
+        tip =
+            Triangle2d ( tipPoint, leftPoint, rightPoint )
+
+        attributes =
+            [ Attributes.stroke "black"
+            , Attributes.fill "white"
+            , Attributes.strokeWidth "0.5"
+            ]
+    in
+        Svg.g attributes [ Svg.lineSegment2d [] stem, Svg.triangle2d [] tip ]
+            |> Svg.placeIn frame
+
+
+frame2d : Frame2d -> Svg msg
+frame2d frame =
+    let
+        originPoint =
+            Frame2d.originPoint frame
+
+        xDirection =
+            Frame2d.xDirection frame
+
+        yDirection =
+            Frame2d.yDirection frame
+    in
+        Svg.g []
+            [ direction2d originPoint xDirection
+            , direction2d originPoint yDirection
+            , Svg.point2d
+                [ Attributes.r "3"
+                , Attributes.stroke "black"
+                , Attributes.strokeWidth "0.5"
+                , Attributes.fill "white"
+                ]
+                originPoint
+            ]
+
+
+placedSvg : Svg Never
+placedSvg =
+    let
+        stampSvg =
+            Svg.polygon2d
+                [ Attributes.fill "orange"
+                , Attributes.stroke "blue"
+                , Attributes.strokeWidth "2"
+                ]
+                (Polygon2d
+                    [ Point2d.origin
+                    , Point2d ( 40, 0 )
+                    , Point2d ( 50, 25 )
+                    , Point2d ( 10, 25 )
+                    ]
+                )
+
+        frames =
+            [ Frame2d.at (Point2d ( 25, 25 ))
+            , Frame2d.at (Point2d ( 100, 25 ))
+            , Frame2d.at (Point2d ( 175, 25 ))
+                |> Frame2d.rotateBy (degrees 20)
+            , Frame2d.at (Point2d ( 25, 150 ))
+            , Frame2d.at (Point2d ( 100, 100 ))
+                |> Frame2d.rotateBy (degrees 20)
+            , Frame2d.at (Point2d ( 150, 150 ))
+                |> Frame2d.rotateBy (degrees -30)
+            ]
+    in
+        Svg.g []
+            [ Svg.g []
+                (List.map frame2d frames)
+            , Svg.g []
+                (List.map (\frame -> Svg.placeIn frame stampSvg) frames)
+            ]
+
+
+examples =
+    [ pointSvg
+    , lineSegmentSvg
+    , triangleSvg
+    , polylineSvg
+    , polygonSvg
+    , scaledSvg
+    , rotatedSvg
+    , translatedSvg
+    , mirroredSvg
+    , placedSvg
+    ]
+
+
+topLeftFrame : Frame2d
+topLeftFrame =
+    Frame2d
+        { originPoint = Point2d ( 0, 300 )
+        , xDirection = Direction2d.x
+        , yDirection = Direction2d.negate Direction2d.y
+        }
+
+
+container : Svg Never -> Html Never
+container svg =
+    Html.div []
+        [ Svg.svg [ Attributes.width "300", Attributes.height "300" ]
+            [ Svg.relativeTo topLeftFrame svg ]
+        ]
+
+
+divider : Html Never
+divider =
+    Html.hr [] []
+
+
+main : Html Never
+main =
+    Html.div [] (List.map container examples |> List.intersperse divider)
--- src/OpenSolid/Svg.elm
+++ src/OpenSolid/Svg.elm
@@ -13,6 +13,74 @@ module OpenSolid.Svg
         , placeIn
         )
 
+{-| Draw SVG using OpenSolid data types. In general, these functions handle
+transforming OpenSolid geometry into SVG elements with the necessary geometric
+attributes, but in most cases you will have to add some non-geometric attributes
+yourself. For example, the `polygon2d` function creates a `<polygon>` element
+with a `points` attribute, but you need to set your own `stroke` and `fill`
+attributes.
+
+# Reading this documentation
+
+For the examples, assume that the following imports are present:
+
+    import Svg exposing (Svg)
+    import Svg.Attributes as Attributes
+    import OpenSolid.Svg as Svg
+    import OpenSolid.Geometry.Types exposing (..)
+
+Also assume that any necessary OpenSolid modules have been imported using the
+following format:
+
+    import OpenSolid.Point2d as Point2d
+
+All examples use a Y-up coordinate system unlike SVG's Y-down (window)
+coordinate system; they were all rendered with a final
+`Svg.relativeTo topLeftFrame` call where
+
+    topLeftFrame =
+        Frame2d
+            { originPoint = Point2d ( 0, 300 )
+            , xDirection = Direction2d.x
+            , yDirection = Direction2d.negate Direction2d.y
+            }
+
+# Geometry
+
+@docs point2d, lineSegment2d, triangle2d, polyline2d, polygon2d
+
+# Transformations
+
+These functions allow you to use all the normal OpenSolid 2D transformations on
+arbitrary bits of SVG. For example,
+
+    Svg.mirrorAcross Axis2d.x
+        (Svg.lineSegment2d [] lineSegment)
+
+is visually the same as
+
+    Svg.lineSegment2d []
+        (LineSegment2d.mirrorAcross Axis2d.x lineSegment)
+
+but the latter will be represented as a simple `<polyline>` in the resulting SVG
+while the former will be represented as a `<polyline>` inside a `<g>` that has a
+`transform` attribute.
+
+If the transformation changes frequently (an animated rotation angle, for
+example) while the geometry itself does not, using an SVG transformation can be
+more efficient since the geometry does not have to be recreated (the SVG virtual
+DOM only has to update the transformation matrix).
+
+@docs scaleAbout, rotateAround, translateBy, mirrorAcross
+
+# Coordinate transformations
+
+Similar to the above transformations, these functions allow OpenSolid coordinate
+conversion transformations to be applied to arbitrary SVG elements.
+
+@docs relativeTo, placeIn
+-}
+
 import String
 import Html exposing (Html)
 import Svg as Svg exposing (Svg, Attribute)
@@ -27,6 +95,22 @@ import OpenSolid.Polyline2d as Polyline2d
 import OpenSolid.Polygon2d as Polygon2d
 
 
+{-| Draw a single point with the given attributes. This is actually drawn as a
+`<circle>` element; the `cx` and `cy` attributes will be set from the given
+point, but you will want to set at least the `r` attribute.
+
+    pointSvg : Svg Never
+    pointSvg =
+        Svg.point2d
+            [ Attributes.r "10"
+            , Attributes.fill "orange"
+            , Attributes.stroke "blue"
+            , Attributes.strokeWidth "2"
+            ]
+            (Point2d ( 150, 150 ))
+
+![point2d](https://opensolid.github.io/images/svg/1.0/pointSvg.png)
+-}
 point2d : List (Attribute msg) -> Point2d -> Svg msg
 point2d attributes point =
     let
@@ -56,6 +140,22 @@ pointsAttribute points =
     Attributes.points (String.join " " (List.map coordinatesString points))
 
 
+{-| Draw a `LineSegment2d` as an SVG `<polyline>` with the given attributes.
+
+    lineSegmentSvg : Svg Never
+    lineSegmentSvg =
+        Svg.lineSegment2d
+            [ Attributes.stroke "blue"
+            , Attributes.strokeWidth "5"
+            ]
+            (LineSegment2d
+                ( Point2d ( 100, 100 )
+                , Point2d ( 200, 200 )
+                )
+            )
+
+![lineSegment2d](https://opensolid.github.io/images/svg/1.0/lineSegmentSvg.png)
+-}
 lineSegment2d : List (Attribute msg) -> LineSegment2d -> Svg msg
 lineSegment2d attributes lineSegment =
     let
@@ -65,6 +165,25 @@ lineSegment2d attributes lineSegment =
         Svg.polyline (pointsAttribute [ p1, p2 ] :: attributes) []
 
 
+{-| Draw a `Triangle2d` as an SVG `<polygon>` with the given attributes.
+
+    triangleSvg : Svg Never
+    triangleSvg =
+        Svg.triangle2d
+            [ Attributes.stroke "blue"
+            , Attributes.strokeWidth "10"
+            , Attributes.strokeLinejoin "round"
+            , Attributes.fill "orange"
+            ]
+            (Triangle2d
+                ( Point2d ( 100, 100 )
+                , Point2d ( 200, 100 )
+                , Point2d ( 100, 200 )
+                )
+            )
+
+![triangle2d](https://opensolid.github.io/images/svg/1.0/triangleSvg.png)
+-}
 triangle2d : List (Attribute msg) -> Triangle2d -> Svg msg
 triangle2d attributes triangle =
     let
@@ -74,6 +193,29 @@ triangle2d attributes triangle =
         Svg.polygon (pointsAttribute [ p1, p2, p3 ] :: attributes) []
 
 
+{-| Draw a `Polyline2d` as an SVG `<polyline>` with the given attributes.
+
+    polylineSvg : Svg Never
+    polylineSvg =
+        Svg.polyline2d
+            [ Attributes.stroke "blue"
+            , Attributes.fill "none"
+            , Attributes.strokeWidth "5"
+            , Attributes.strokeLinecap "round"
+            , Attributes.strokeLinejoin "bevel"
+            ]
+            (Polyline2d
+                [ Point2d ( 100, 100 )
+                , Point2d ( 120, 200 )
+                , Point2d ( 140, 100 )
+                , Point2d ( 160, 200 )
+                , Point2d ( 180, 100 )
+                , Point2d ( 200, 200 )
+                ]
+            )
+
+![polyline2d](https://opensolid.github.io/images/svg/1.0/polylineSvg.png)
+-}
 polyline2d : List (Attribute msg) -> Polyline2d -> Svg msg
 polyline2d attributes polyline =
     let
@@ -83,6 +225,25 @@ polyline2d attributes polyline =
         Svg.polyline (pointsAttribute vertices :: attributes) []
 
 
+{-| Draw a `Polygon2d` as an SVG `<polygon>` with the given attributes.
+
+    polygonSvg : Svg Never
+    polygonSvg =
+        Svg.polygon2d
+            [ Attributes.stroke "blue"
+            , Attributes.fill "orange"
+            , Attributes.strokeWidth "5"
+            ]
+            (Polygon2d
+                [ Point2d ( 100, 200 )
+                , Point2d ( 120, 150 )
+                , Point2d ( 180, 150 )
+                , Point2d ( 200, 200 )
+                ]
+            )
+
+![polygon2d](https://opensolid.github.io/images/svg/1.0/polygonSvg.png)
+-}
 polygon2d : List (Attribute msg) -> Polygon2d -> Svg msg
 polygon2d attributes polygon =
     let
@@ -92,6 +253,26 @@ polygon2d attributes polygon =
         Svg.polygon (pointsAttribute vertices :: attributes) []
 
 
+{-| Scale arbitrary SVG around a given point by a given scale.
+
+    scaledSvg : Svg Never
+    scaledSvg =
+        let
+            scales =
+                [ 1.0, 1.5, 2.5 ]
+
+            referencePoint =
+                Point2d ( 100, 100 )
+
+            scaledPoint : Float -> Svg Never
+            scaledPoint scale =
+                Svg.scaleAbout referencePoint scale pointSvg
+        in
+            Svg.g []
+                (crosshair referencePoint :: List.map scaledPoint scales)
+
+![scaleAbout](https://opensolid.github.io/images/svg/1.0/scaledSvg.png)
+-}
 scaleAbout : Point2d -> Float -> Svg msg -> Svg msg
 scaleAbout point scale element =
     let
@@ -107,26 +288,158 @@ scaleAbout point scale element =
         Svg.g [ Attributes.transform transform ] [ element ]
 
 
+{-| Rotate arbitrary SVG around a given point by a given angle.
+
+    rotatedSvg : Svg Never
+    rotatedSvg =
+        let
+            angles =
+                List.map degrees [ 0, 30, 60, 90, 120, 150 ]
+
+            referencePoint =
+                Point2d ( 200, 150 )
+
+            rotatedPoint : Float -> Svg Never
+            rotatedPoint angle =
+                Svg.rotateAround referencePoint angle pointSvg
+        in
+            Svg.g []
+                (crosshair referencePoint :: List.map rotatedPoint angles)
+
+![rotateAround](https://opensolid.github.io/images/svg/1.0/rotatedSvg.png)
+-}
 rotateAround : Point2d -> Float -> Svg msg -> Svg msg
 rotateAround point angle =
     placeIn (Frame2d.rotateAround point angle Frame2d.xy)
 
 
+{-| Translate arbitrary SVG by a given displacement.
+
+    translatedSvg : Svg Never
+    translatedSvg =
+        Svg.g []
+            [ polylineSvg
+            , Svg.translateBy (Vector2d ( 0, 40 )) polylineSvg
+            , Svg.translateBy (Vector2d ( 5, -60 )) polylineSvg
+            ]
+
+![translateBy](https://opensolid.github.io/images/svg/1.0/translatedSvg.png)
+-}
 translateBy : Vector2d -> Svg msg -> Svg msg
 translateBy vector =
     placeIn (Frame2d.translateBy vector Frame2d.xy)
 
 
+{-| Mirror arbitrary SVG across a given axis.
+
+    mirroredSvg : Svg Never
+    mirroredSvg =
+        let
+            horizontalAxis =
+                Axis2d
+                    { originPoint = Point2d ( 0, 220 )
+                    , direction = Direction2d.x
+                    }
+
+            angledAxis =
+                Axis2d
+                    { originPoint = Point2d ( 0, 150 )
+                    , direction = Direction2d.fromAngle (degrees -10)
+                    }
+        in
+            Svg.g []
+                [ polygonSvg
+                , axis2d horizontalAxis
+                , Svg.mirrorAcross horizontalAxis polygonSvg
+                , axis2d angledAxis
+                , Svg.mirrorAcross angledAxis polygonSvg
+                ]
+
+![mirrorAcross](https://opensolid.github.io/images/svg/1.0/mirroredSvg.png)
+-}
 mirrorAcross : Axis2d -> Svg msg -> Svg msg
 mirrorAcross axis =
     placeIn (Frame2d.mirrorAcross axis Frame2d.xy)
 
 
+{-| Convert SVG expressed in global coordinates to SVG expressed in coordinates
+relative to a given reference frame. Using `relativeTo` in convert with
+`scaleAbout` can be useful for transforming between model space and screen space
+(SVG native coordinates start in the top left, so positive Y is down).
+
+For example, you might develop an SVG scene in a coordinate system where X and Y
+each range from -2 to 2 and positive Y is up. To turn this into (say) a 400x400
+SVG drawing, first define the SVG frame (coordinate system) in terms of your
+model coordinate system:
+
+    topLeftFrame =
+        Frame2d
+            { originPoint = Point2d ( -2, -2 )
+            , xDirection = Direction2d.x
+            , yDirection = Direction2d.flip Direction2d.y
+            }
+
+(As expressed in your model frame, the top-left SVG frame is at the point
+(-2, -2) and has the global negative Y direction as its Y direction.) If
+`scene` is an SVG element representing your scene, you can then transform it
+into top-left SVG window coordinates using
+
+    scene
+        |> Svg.relativeTo topLeftFrame
+        |> Svg.scaleAbout Point2d.origin 100
+
+(with the scaling required because otherwise your drawing would be 4 pixels by
+4 pixels, not 400 by 400).
+-}
 relativeTo : Frame2d -> Svg msg -> Svg msg
 relativeTo frame =
     placeIn (Frame2d.relativeTo frame Frame2d.xy)
 
 
+{-| Take SVG defined in local coordinates relative to a given reference frame,
+and return that SVG expressed in global coordinates.
+
+This can be useful for taking a chunk of SVG and 'stamping' it in different
+positions with different orientations:
+
+    placedSvg : Svg Never
+    placedSvg =
+        let
+            stampSvg =
+                Svg.polygon2d
+                    [ Attributes.fill "orange"
+                    , Attributes.stroke "blue"
+                    , Attributes.strokeWidth "2"
+                    ]
+                    (Polygon2d
+                        [ Point2d.origin
+                        , Point2d ( 40, 0 )
+                        , Point2d ( 50, 25 )
+                        , Point2d ( 10, 25 )
+                        ]
+                    )
+
+            frames =
+                [ Frame2d.at (Point2d ( 25, 25 ))
+                , Frame2d.at (Point2d ( 100, 25 ))
+                , Frame2d.at (Point2d ( 175, 25 ))
+                    |> Frame2d.rotateBy (degrees 20)
+                , Frame2d.at (Point2d ( 25, 150 ))
+                , Frame2d.at (Point2d ( 100, 100 ))
+                    |> Frame2d.rotateBy (degrees 20)
+                , Frame2d.at (Point2d ( 150, 150 ))
+                    |> Frame2d.rotateBy (degrees -30)
+                ]
+        in
+            Svg.g []
+                [ Svg.g []
+                    (List.map frame2d frames)
+                , Svg.g []
+                    (List.map (\frame -> Svg.placeIn frame stampSvg) frames)
+                ]
+
+![placeIn](https://opensolid.github.io/images/svg/1.0/placedSvg.png)
+-}
 placeIn : Frame2d -> Svg msg -> Svg msg
 placeIn frame element =
     let
